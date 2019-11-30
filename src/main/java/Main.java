dd6b6d2eb52e654562034a211c2b79d59fc9d13b
josecl200 <20160138@ce.pucmm.edu.do>
2019-11-30 08:36:11 -0400

    changed port and added jar generation for AWS deployment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, NoSuchAlgorithmException {
-        Spark.port(8081);
+        Spark.port(42069);
         Bootstrap.startDb();
         DBCon.getInstance().testConnection();
         Spark.staticFileLocation("/public");
